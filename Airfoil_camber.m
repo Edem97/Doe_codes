97bc491db643ec9be8fd151f9552092fb806d6c0
Edem97 <99991625+Edem97@users.noreply.github.com>
2022-06-16 05:09:38 +0000

    Update Airfoil_camber.m
    This Matlab code gives a user an opportunity to generate a NACA Airfoil for aerodynamic analysis and force calculation for aircraft wing design.
    The user is required to specify a maximum camber, position of maximum camber and maximum thickness of the desired airfoil structure.
    
    A NACA 2412 was used as a case study.

--- Airfoil_camber.m
+++ Airfoil_camber.m
@@ -10,7 +10,7 @@ y1 = 5.* t.*( (0.2969 .*sqrt(xa)) - (0.126.*xa) - (0.3516.*xa.^2) + (0.2843.*xa.
 ya = Y1 + y1;
 plot(xa,ya)
 
-%% This is part two of the airfoil 
+%% This is part two of the airfoil.
 xb = m:0.001:1;
 Y2 = (c ./ (1 -m)^2) .*(1 - (2.*m) + (2.*m.*xb) - (xb.^2) ) ; %% camber line part two
 y1 = 5.* t.*( (0.2969 .*sqrt(xb)) - (0.126.*xb) - (0.3516.*xb.^2) + (0.2843.*xb.^3)- (0.1015.*xb.^4) );
